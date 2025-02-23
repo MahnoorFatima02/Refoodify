47b3bd6c369d621bb4ce9343b8dc4876aff20649
Mahnoor Fatima <89933726+MahnoorFatima02@users.noreply.github.com>
2025-02-23 21:33:47 +0200

    render url added

--- Frontend/src/redux/axiosInstance.js
+++ Frontend/src/redux/axiosInstance.js
@@ -3,7 +3,7 @@ import { store } from "./store";
 import { refreshAccessToken } from "./user/actions";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://refoodifybackend.onrender.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
